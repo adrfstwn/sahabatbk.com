f764b52ef8a06e2add6030abf65f5f51ec1fbdbf
Ade Rifky Setiawan <100577989+adrfstwn@users.noreply.github.com>
2025-06-30 11:07:07 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(
-            proxies: '*', // ← percaya semua proxy (nginx, docker)
+            at: '*',
             headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
         );
     })
